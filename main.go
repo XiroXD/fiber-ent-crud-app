4ccadd4aaae4c134355ef11ac00cb8f941939af6
Xiro <xiro@duck.com>
2023-12-22 22:51:37 +0100

    feat: estabilished database connection

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/XiroXD/fiber-ent-crud-app/provider/db"
+	"github.com/gofiber/fiber/v2"
+)
 
+func init() {
+	db.Connect()
+}
 func main() {
 	app := fiber.New()
 
