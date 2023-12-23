1c0270462991949f9392169f27a6ac245832564e
Xiro <xiro@duck.com>
2023-12-23 15:45:27 +0100

    feat: Graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
-	"github.com/XiroXD/fiber-ent-crud-app/provider/db"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/XiroXD/fiber-ent-crud-app/provider/db"
 )
 
 func init() {
@@ -10,13 +17,30 @@ func init() {
 }
 func main() {
 	server()
-
-	// Close database connection before exit
-	defer db.Disconnect()
 }
 
 func server() {
 	app := fiber.New()
 
-	app.Listen(":3000")
+	// Listen from a different goroutine
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	// Create channel to signify a signal being sent
+	c := make(chan os.Signal, 1)
+	// When an interrupt or termination signal is sent, notify the channel
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	_ = <-c //lint:ignore S1005 Blocks main thread until an interrupt is received
+	fmt.Println("\nGracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+
+	db.Disconnect()
+	fmt.Println("Fiber was successful shutdown.")
+
 }
