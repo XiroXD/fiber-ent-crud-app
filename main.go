3ea71796b9d7ae42b7054f53883517f24608d76b
Xiro <xiro@duck.com>
2023-12-23 15:24:51 +0100

    refactor: Remove testing stuff

--- main.go
+++ main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"context"
-
 	"github.com/XiroXD/fiber-ent-crud-app/provider/db"
 	"github.com/gofiber/fiber/v2"
 )
 
 func init() {
 	db.Connect()
-
-	ctx := context.Background()
-
-	cat, _ := db.GetClient().Pet.Create().SetName("Garfield").SetBreed("Maine Coon").SetAge(5).Save(ctx)
-	dog, _ := db.GetClient().Pet.Create().SetName("Odie").SetBreed("Beagle").SetAge(3).Save(ctx)
-
-	db.GetClient().User.Create().SetName("Jon Arbuckle").SetEmail("JonArbuckle@example.com").SetPassword("password").AddPets(cat, dog).Save(ctx)
 }
 func main() {
 	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		u, _ := db.GetClient().User.Query().WithPets().Only(c.Context())
-
-		return c.JSON(u)
-	})
 
 	app.Listen(":3000")
 }
