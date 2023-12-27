c18de8166438b73bbd73b54e64b0fdc47a725043
Xiro <xiro@duck.com>
2023-12-27 18:28:05 +0100

    feat: user creation endpoint

--- api/controllers/user_controller.go
+++ api/controllers/user_controller.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
-	"github.com/XiroXD/fiber-ent-crud-app/api/services"
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/XiroXD/fiber-ent-crud-app/api/services"
+	"github.com/XiroXD/fiber-ent-crud-app/schema"
 )
 
 type UsersController struct{}
@@ -13,4 +15,14 @@ func NewUsersController() *UsersController {
 
 func (c *UsersController) Index(ctx *fiber.Ctx) error {
 	return services.Hello(ctx)
+}
+
+func (c *UsersController) Create(ctx *fiber.Ctx) error {
+	b := new(schema.CreateUser)
+
+	if err := ctx.BodyParser(b); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(err)
+	}
+
+	return services.Create(ctx, b.Name, b.Email, b.Password)
 }
--- api/services/user_service.go
+++ api/services/user_service.go
@@ -1,7 +1,29 @@
 package services
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/XiroXD/fiber-ent-crud-app/hash"
+	"github.com/XiroXD/fiber-ent-crud-app/provider/db"
+)
 
 func Hello(ctx *fiber.Ctx) error {
 	return ctx.JSON(fiber.Map{"message": "Hello, World!"})
+}
+
+func Create(ctx *fiber.Ctx, name string, email string, password string) error {
+	c := ctx.Context()
+
+	hash := hash.CreateHash(password)
+
+	user, err := db.GetClient().User.Create().
+		SetName(name).
+		SetEmail(email).
+		SetPassword(string(hash)).
+		Save(c)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	return ctx.JSON(user)
 }
